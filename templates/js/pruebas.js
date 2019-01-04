abc82c022bd3141dbad91e768593f082f35e054f
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-01-03 20:21:22 -0400

    Conversión de data a DOM

--- templates/js/pruebas.js
+++ templates/js/pruebas.js
@@ -1,4 +1,4 @@
-let dao=new AuthManager();
+/*let dao=new AuthManager();
 let b=new EventManager();
 let c =new UserDAO();
 async function fun()
@@ -9,3 +9,60 @@ async function fun()
     console.log(await b.attendToEventWithID(3));
 }
 //fun();
+*/
+
+
+function getNameOfDay(date)
+{
+    const d=["Domingo", "Lunes","Martes", "Miércoles", "Jueves","Viernes", "Sábado" ];
+    return d[date.getDay()];
+}
+function getNameOfMonth(date)
+{
+    const d=["Enero","Febrero", "Marzo", "Abril","Mayo", "Junio", "Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    return d[date.getMonth()];
+}
+class CardEvent
+{
+    constructor(event, i)
+    {
+        i++;
+        this.changeDay(getNameOfDay(event.date),i);
+        this.changeMonth(getNameOfMonth(event.date),i);
+        this.changeNumberDay(event.date.getDate(),i);
+        this.changeTitle(event.title, i);
+        this.changeCover(event.cover, i);
+        console.log(event.cover);
+    }
+    changeDay(day, i)
+    {
+        $(".card"+i).find(".text-day.day").text(day);
+    }
+    changeMonth(month, i)
+    {
+        $(".card"+i).find(".text-day.month").text(month);
+    }
+    changeNumberDay(number,i)
+    {
+        $(".card"+i).find(".number-day").text(number);
+    }
+
+    changeTitle(title, i)
+    {
+        $(".card"+i).find(".title-event-card").text(title);
+    }
+
+    changeCover(cover, i)
+    {
+        $(".card"+i).find(".card").css("background-image", 'url('+cover+')');
+    }
+}
+function prueba()
+{
+    let date=new Date("2019-01-05T10:10:00");
+    console.log(date);
+    let event=new Event(1, 1, date, 1, "Maratón de programación", "", "https://www.coalitionagency.com/wp-content/uploads/Coalition-Event-Background.jpg", "", "");
+    new CardEvent(event,0);
+}
+
+prueba();
